9c2da40d4986dbc8cc9d33515b76dce2c0336eed
Leonardo Almeida <leoalmeidasa@gmail.com>
2021-12-10 17:42:17 -0300

    feat: Citizen create update and show

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 class Citizen < ApplicationRecord
+  has_one :address
   validates :name, :cpf, :cns, :email, :birth_date, :telephone, :status, presence: true
-  validates :cpf, length: {minimum: 11, maximum: 11}, uniqueness: true
-  validates :cns, length: {minimum: 15, maximum: 15}, uniqueness: true
+  validates :cpf, length: { minimum: 11, maximum: 11 }, uniqueness: true
+  validates :cns, length: { minimum: 15, maximum: 15 }, uniqueness: true
   validates :email, format: { with: /\A.*@.*\.com\z/ }
   has_one_attached :photo
 
-  scope :active, -> { where(status: true) }
-  scope :inactive, -> { where(status: false) }
 end
--- app/views/citizens/_citizen.json.jbuilder
+++ app/views/citizens/_citizen.json.jbuilder
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 json.extract! citizen, :id, :name, :cpf, :cns, :email, :birth_date, :telephone, :status, :created_at, :updated_at
 json.url citizen_url(citizen, format: :json)
