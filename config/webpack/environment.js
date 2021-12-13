1666c9ea4634338fc9298548b84c3bf76a4f9e96
Leonardo Almeida <leoalmeidasa@gmail.com>
2021-12-13 09:36:06 -0300

    refactor: remove node_modules and yarn.lock

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,30 +1,3 @@
-// config/webpack/environment.js
 const { environment } = require('@rails/webpacker')
-const globImporter = require('node-sass-glob-importer');
-
-environment
-    .loaders
-    .get('sass')
-    .use
-    .find(item => item.loader === 'sass-loader')
-    .options = { sassOptions: { importer: globImporter() } }; // <-- this!
-
-const webpack = require('webpack')
-
-environment.plugins.append('Provide', new webpack.environment({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-}),
-    // Adds window.$ = require('jquery')
-    environment.loaders.append('jquery', {
-        test: require.resolve('jquery'),
-        use: [
-            {
-                loader: 'expose-loader',
-                options: '$',
-            },
-        ],
-    }))
 
 module.exports = environment
